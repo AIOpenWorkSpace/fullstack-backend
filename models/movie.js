4b87572f08f8dd0d2b9a4d411aa77576ad193a7e
Aaron Clark <amclarkwustl@gmail.com>
2023-06-26 16:23:27 -0700

    added parsing function

--- models/movie.js
+++ models/movie.js
@@ -3,23 +3,52 @@
 const mongoose =require('mongoose');
 const { Schema } = mongoose;
 
+function parseMovieData(title, data) {
+  const startIndex = data.indexOf('Language Usage');
+  const stripped = data.substring(startIndex);
+  console.log(stripped);
+  const sections = stripped.split ('\n\n');
+
+  return {
+    title: title,
+    languageDescription: sections[0],
+    drugDescription: sections[1],
+    sexDescription: sections[2],
+    roleModelDescription: sections[3],
+    messageDescription: sections[4],
+    representationDescription: sections[5],
+    violenceDescription: sections[6],
+    productDescription: sections[7],
+  };
+}
+
+// async function getPoster(req, res, next) {
+//   try {
+//     let movie=request.query
+//   } catch () {
+    
+//   }
+// }
+
+
+
 const movieSchema = new Schema ({
   title: { type: String, required: true },
-  languageRating: {type: Number, required: true},
+  languageRating: {type: Number},
   languageDescription: { type: String, required: true },
-  drugRating: {type: Number, required: true},
+  drugRating: {type: Number},
   drugDescription: { type: String, required: true },
-  sexRating: {type: Number, required: true},
+  sexRating: {type: Number},
   sexDescription: { type: String, required: true },
-  roleModelRating: {type: Number, required: true},
+  roleModelRating: {type: Number},
   roleModelDescription: { type: String, required: true },
-  messageRating: {type: Number, required: true},
+  messageRating: {type: Number},
   messageDescription: { type: String, required: true },
-  representationRating: {type: Number, required: true},
+  representationRating: {type: Number},
   representationDescription: { type: String, required: true },
-  violenceRating: {type: Number, required: true},
+  violenceRating: {type: Number},
   violenceDescription: { type: String, required: true },
-  productRating: {type: Number, required: true},
+  productRating: {type: Number},
   productDescription: { type: String, required: true },
   user: { type: String},
   imageUrl: { type: String}
@@ -27,4 +56,4 @@ const movieSchema = new Schema ({
 
 const Movie = mongoose.model('movie', movieSchema);
 
-module.exports = Movie;
+module.exports = {Movie, parseMovieData};
